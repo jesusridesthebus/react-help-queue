23b376b5d6e709d0672bdfbc05d097e6c80df8a4
Student <example@example.com>
2019-07-02 16:48:04 -0700

    add styling

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
-  var ticketStyles = {
-    backgroundColor: '#ecf0f1',
-    fontFamily: 'sans-serif',
-    paddingTop: '50px'
-  }
   return (
-      <div style={ticketStyles}>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
-   );
+    <div>
+        <style global jsx>{`
+          div {
+            background-color: red;
+          }
+        `}</style>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 Ticket.propTypes = {

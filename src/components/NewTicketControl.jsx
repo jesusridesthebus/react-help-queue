65542dcecf7f14217c704a68fc972ebab6fd836a
Jared Farkas and Liz Kelley <liz@>
2019-07-09 16:52:27 -0700

    set up linting

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 class NewTicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
     };
     this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
   }

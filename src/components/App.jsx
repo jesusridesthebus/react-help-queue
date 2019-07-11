dff332084aed9a761b2258d594150df716a45aa0
Jared Farkas and Liz Kelley <liz@>
2019-07-11 09:10:38 -0700

    add admin component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
 import Moment from 'moment';
+import Admin from './Admin';
 
 class App extends React.Component {
 
@@ -16,44 +17,43 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     this.waitTimeUpdateTimer = setInterval(() =>
     this.updateTicketElapsedWaitTime(),
     60000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime() {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+    ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
   );
-}
-
-componentWillUnmount(){
-  console.log('componentWillUnmount');
-  clearInterval(this.waitTimeUpdateTimer);
-}
-
-updateTicketElapsedWaitTime() {
-  let newMasterTicketList = this.state.masterTicketList.slice();
-  newMasterTicketList.forEach((ticket) =>
-  ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
-);
-this.setState({masterTicketList: newMasterTicketList})
-}
-
-handleAddingNewTicketToList(newTicket){
-  var newMasterTicketList = this.state.masterTicketList.slice();
-  newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
-  newMasterTicketList.push(newTicket);
   this.setState({masterTicketList: newMasterTicketList});
-}
+  }
 
-render(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
-        <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
-      </Switch>
-    </div>
-  );
-}
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname} />} />
+        </Switch>
+      </div>
+    );
+  }
 
 }
 
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -3,26 +3,34 @@ import PropTypes from 'prop-types';
 import Moment from 'moment';
 
 function Ticket(props){
-  return (
-    <div>
-      <style global jsx>{`
-        div {
-          background-color: yellow;
-        }
-      `}</style>
-      <h3>{props.location} - {props.names}</h3>
-      <h4>{props.formattedWaitTime}</h4>
-      <p><em>{props.issue}</em></p>
-      <hr/>
-    </div>
-  );
+  const ticketInformation =
+  <div>
+    <h3>{props.location} - {props.names}</h3>
+    <h4>{props.formattedWaitTime}</h4>
+    <p><em>{props.issue}</em></p>
+    <hr/>
+  </div>;
+  if (props.currentRouterPath === '/admin'){
+    return (
+      <div onClick={() => {alert('hey, you just clicked the ticket belonging to ' + props.names);}}>
+        {ticketInformation}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {ticketInformation}
+      </div>
+    );
+  }
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-  formattedWaitTime: PropTypes.string.isRequired
+  formattedWaitTime: PropTypes.string.isRequired,
+  currentRouterPath: PropTypes.string
 };
 
 export default Ticket;
